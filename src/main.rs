ec65a797cd50845c03d3ccceae1e3780e7c69a3c
Haris Khan <81975456+Haris-AK@users.noreply.github.com>
2023-06-16 08:42:46 +0100

    feat: parse arguments
    Allows the program to take in CLI arguments, handles any errors regarding the arguments such as length of if they are a valid path, and if they are valid, it returns the contents of the file they have chosen.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// Used to read CLI arguments
+use std::env;
+// Used to access the filesystem
+use std::process;
+
+// We can import as
+use minigrep::{run, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing arguments: {}", err);
+        // Instead of exiting the program with a panic error, we use a nonzero error code which is
+        // convention to signal that a process that called our program had the program exited with
+        // an error state
+        process::exit(1) });
+    if let Err(e) = run(config) {
+        println!("Application error {}", e);
+        process::exit(1)
+    }
 }
